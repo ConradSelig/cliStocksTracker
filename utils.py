1944cc660b28b8dd0dd674ddddfbf36a48c8beb1
Yonatan Maor <yon.maor@gmail.com>
2021-02-20 10:33:47 +0200

    Added new rounding functionality

---
+++ utils.py
@@ -0,0 +1,20 @@
+from math import trunc
+
+#Rounds value down to the desired number of decimals digits (controlled decimal_places) using math or truncate mode
+def round_value(value, mode, decimal_places):
+    if mode == "math":
+        return round(value, decimal_places)
+    elif mode == "down":
+        # Check that decimal_places is a non-negative integer
+        if not(type(decimal_places)==int):
+            raise TypeError("The number of decimal places has to be an integer")
+        elif decimal_places < 0:
+            raise ValueError("The number of decimal places has to be positive")
+        elif decimal_places == 0:
+            return trunc(value, decimal_places)
+        
+        factor = 10.0 ** decimal_places
+        return trunc(value * factor) / factor
+        
+print(round_value("down", 1.59473, 3))
+        
