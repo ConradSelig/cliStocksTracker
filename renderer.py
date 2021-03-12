e38f95d24473fd69b563ffb448eb6b22ee629215
Patrick Hovsepian <phovsepian@quickbase.com>
2021-03-11 23:22:09 -0700

    add color support

--- renderer.py
+++ renderer.py
@@ -2,7 +2,7 @@ import io
 import utils
 import portfolio
 
-from colorama import Fore, Style
+from colorama import Fore, Style, Back
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 from typing import List, Callable
@@ -11,7 +11,7 @@ from typing import List, Callable
 @dataclass
 class CellData:
     value: str
-    color: str = None
+    color: str = Fore.RESET
 
 @dataclass
 class ColumnFormatter:
@@ -38,8 +38,8 @@ def format_gl(value: float, is_currency: bool = True) -> str:
 _stock_column_formatters = {
     "Ticker" : ColumnFormatter("Ticker", 9, lambda stock: CellData(stock.symbol)),
     "Current Price" : ColumnFormatter("Last", 12, lambda stock: CellData(format_number(stock.curr_value))),
-    "Daily Change Amount": ColumnFormatter("Chg", 12, lambda stock: CellData(format_gl(stock.change_amount))),
-    "Daily Change Percentage": ColumnFormatter("Chg%", 10, lambda stock: CellData(format_gl(stock.change_percentage, False))),
+    "Daily Change Amount": ColumnFormatter("Chg", 12, lambda stock: CellData(format_gl(stock.change_amount), Fore.GREEN if stock.change_amount >= 0 else Fore.RED)),
+    "Daily Change Percentage": ColumnFormatter("Chg%", 10, lambda stock: CellData(format_gl(stock.change_percentage, Fore.GREEN if stock.change_percentage >= 0 else Fore.RED))),
     "Low": ColumnFormatter("Low", 12, lambda stock: CellData(format_number(stock.low))),
     "High": ColumnFormatter("High", 12, lambda stock: CellData(format_number(stock.high))),
     "Daily Average Price": ColumnFormatter("Avg", 12, lambda stock: CellData(format_number(stock.average))),
@@ -47,10 +47,10 @@ _stock_column_formatters = {
 
 _portfolio_column_formatters = {
     "Stocks Owned": ColumnFormatter("Owned", 9, lambda entry: CellData(format_number(entry.count))),
-    "Gains per Share": ColumnFormatter("G/L/S", 12, lambda entry: CellData(format_gl(entry.gains_per_share))),
+    "Gains per Share": ColumnFormatter("G/L/S", 12, lambda entry: CellData(format_gl(entry.gains_per_share), Fore.GREEN if entry.gains_per_share >= 0 else Fore.RED)),
     "Current Market Value": ColumnFormatter("Mkt V", 12, lambda entry: CellData(format_number(entry.holding_market_value))),
     "Average Buy Price": ColumnFormatter("Buy", 12, lambda entry: CellData(format_number(entry.average_cost))),
-    "Total Share Gains": ColumnFormatter("G/L/T", 12, lambda entry: CellData(format_gl(entry.gains))),
+    "Total Share Gains": ColumnFormatter("G/L/T", 12, lambda entry: CellData(format_gl(entry.gains), Fore.GREEN if entry.gains >= 0 else Fore.RED)),
     "Total Share Cost": ColumnFormatter("Cost", 12, lambda entry: CellData(format_number(entry.cost_basis))),
 }
 
@@ -58,7 +58,6 @@ class Renderer(metaclass=utils.Singleton):
     def __init__(self, rounding: str, portfolio: portfolio.Portfolio, *args, **kwargs):
         self.mode = rounding
         self.portfolio = portfolio
-        self.cell_width = 13
         return
 
     def render(self):
@@ -68,60 +67,6 @@ class Renderer(metaclass=utils.Singleton):
 
         self.print_new_table()
         return
-
-    """
-    def print_ticker_summaries(self, format_str, table):
-        for line in table:
-            if line[-1] is None:
-                pass
-            else:
-                print(Fore.GREEN if line[-1] else Fore.RED, end="")
-
-            print("\t" + "".join([format_str.format(item) for item in line[:-1]]))
-            print(Style.RESET_ALL, end="")
-
-        # print the totals line market value then cost
-        print("{:126}".format("\nTotals: "), end="")
-
-        # current market value
-        print(
-            Fore.GREEN
-            if self.portfolio.market_value >= self.portfolio.cost_value
-            else Fore.RED,
-            end="",
-        )
-        print(
-            format_str.format(
-                "$" + self.format_number(self.portfolio.market_value)
-            ),
-            end="",
-        )
-        print(Fore.RESET, end="")
-
-        # G/L Total
-        print(
-            Fore.GREEN
-            if self.portfolio.market_value >= self.portfolio.cost_value
-            else Fore.RED,
-            end="",
-        )
-        print(
-            "{:13}".format("")
-            + format_str.format(
-                self.format_gl(self.portfolio.market_value - self.portfolio.cost_value, True)
-            ),
-            end="",
-        )
-        print(Fore.RESET, end="")
-
-        # portfolio cost total
-        print(
-            format_str.format(
-                "$" + self.format_number(self.portfolio.cost_value)
-            )
-        )
-        return
-    """
 
     def print_gains(self, format_str, gain, timespan):
         positive_gain = gain >= 0
@@ -189,20 +134,28 @@ class Renderer(metaclass=utils.Singleton):
         print(heading + "\n" + divider)
 
         # now print every portfolio entry
-        for entry in self.portfolio.stocks.values():
-            line = "\t"
+        for i, entry in enumerate(self.portfolio.stocks.values()):
             stock = entry.stock
-            
+            line = "\t"
+
+            highlight_color = Back.LIGHTBLACK_EX if i % 2 == 0 else Back.RESET
+            line += highlight_color
+
             for i, col in enumerate(stock_cols + portfolio_cols):
                 col_formatter = _stock_column_formatters.get(col) 
                 
                 is_stock = col_formatter != None
                 if not is_stock:
                     col_formatter = _portfolio_column_formatters.get(col)
-                line += ("{:" + str(col_formatter.width) + "}").format(col_formatter.generate_string(stock if is_stock else entry))
+
+                cell_data = col_formatter.generator(stock if is_stock else entry)
+                line += cell_data.color + ("{:" + str(col_formatter.width) + "}").format(cell_data.value)
 
             # print the entry
+            line += Style.RESET_ALL
             print(line)
 
+        # TODO: print totals line
+        
         self.print_overall_summary()        
         return
